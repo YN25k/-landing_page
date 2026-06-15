a9ca2f92f2dda1987cc5731548a818ea3cfb8c6f
NickyoyoSu <smyzs4@nottingham.ac.uk>
2026-06-15 11:06:08 +0800

    fix: correct GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/-landing_page/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/zhidian/' : '/',
 })
